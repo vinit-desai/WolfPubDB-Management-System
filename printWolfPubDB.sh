7364fc110ec417ec109fc4087c654f076a00d2ab
Mitchell Young <mgyoung@ncsu.edu>
2022-03-11 11:51:38 -0500

    Modified selects for two tables

--- printWolfPubDB.sh
+++ printWolfPubDB.sh
@@ -8,28 +8,38 @@ database="mgyoung"
 
 # declare an array of table names
 declare -a tables=(
-	"Wages"
-	"Bills"
-	"Transaction"
-	"Orders"
-	"Distributor"
-	"Address"
-	"AuthorsArticle"
-	"AuthorsBook"
-	"Edits"
-	"Contributor"
-	"Article"
-	"Chapter"
-	"Periodical"
-	"Periodicity"
-	"Book"
 	"Publication"
+	"Book"
+	"Periodicity"
+	"Periodical"
+	"Chapter"
+	"Article"
+	"Contributor"
+	"Edits"
+	"AuthorsBook"
+	"AuthorsArticle"
+	"Address"
+	"Distributor"
+	"Orders"
+	"Transaction"
+	"Bills"
+	"Wages"
 )
+
+# manually do chapter and article to avoid printing text
+chapter_query="SELECT PublicationID, ChapterNumber, Title, CONCAT(LEFT(TEXT,32),'...') AS Text FROM Chapter;"
+article_query="SELECT PublicationID, SequenceNumber, Title, CreationDate, CONCAT(LEFT(TEXT,32),'...')  AS Text FROM Article;"
 
 ## now loop through the above array
 for table in "${tables[@]}"
 do
-	query="SELECT * FROM ${table};"
+	if [ "$table" = "Chapter" ]; then
+		query="$chapter_query"
+	elif [ "$table" = "Article" ]; then
+		query="$article_query"
+	else
+		query="SELECT * FROM ${table};"
+	fi
 	echo 
 	echo "${query}"
 	echo
