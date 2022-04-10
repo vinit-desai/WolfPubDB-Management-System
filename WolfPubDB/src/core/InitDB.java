f4bdbc0074e65e09c272368fa83cf3576ec39563
Mitchell Young <mgyoung@ncsu.edu>
2022-04-10 17:43:01 -0400

    Finished adding demo data, including new columns for some tables

--- WolfPubDB/src/core/InitDB.java
+++ WolfPubDB/src/core/InitDB.java
@@ -90,11 +90,11 @@ public class InitDB {
 		/* ------------------------------------------------------------------ */
 		SQL = 
 			"CREATE TABLE Book(" + "\n" +
-			"	PublicationID 	INT 		NOT NULL," + "\n" + 
-			"	ISBN 		CHAR(10) 	NOT NULL," + "\n" + 
-			"	Title 		VARCHAR(128) 	NOT NULL," + "\n" + 
-			"	Edition 	INT 		NOT NULL," + "\n" + 
-			"	CreationDate 	DATE 		NOT NULL," + "\n" + 
+			"	PublicationID 	INT 			NOT NULL," + "\n" + 
+			"	ISBN 			CHAR(10) 		NOT NULL," + "\n" + 
+			"	Title 			VARCHAR(128) 	NOT NULL," + "\n" + 
+			"	Edition 		INT 			NOT NULL," + "\n" + 
+			"	CreationDate 	DATE 			NOT NULL," + "\n" + 
 			"	UNIQUE(ISBN)," 	+ "\n" + 
 			"	PRIMARY KEY (PublicationID)," + "\n" + 
 			"	FOREIGN KEY (PublicationID)" + "\n" + 
@@ -112,7 +112,7 @@ public class InitDB {
 		/* ------------------------------------------------------------------ */
 		SQL = 
 			"CREATE TABLE Periodicity (" + "\n" + 
-			"	Type 		VARCHAR(16)	NOT NULL," + "\n" +
+			"	Type 			VARCHAR(16)		NOT NULL," + "\n" +
 			"	Periodicity 	VARCHAR(16) 	NOT NULL," + "\n" +
 			"	PRIMARY KEY (Type)" + "\n" +
 			");" + "\n" + "\n" 
@@ -126,11 +126,11 @@ public class InitDB {
 		/* ------------------------------------------------------------------ */
 		SQL = 
 			"CREATE TABLE Periodical (" + "\n" + 
-			"	PublicationID 	INT 		NOT NULL," + "\n" +
-			"	Type 		VARCHAR(16) 	NOT NULL," + "\n" + 
-			"	Title 		VARCHAR(128) 	NOT NULL," + "\n" + 
-			"	Issue 		INT 		NOT NULL," + "\n" + 
-			"	IssueDate 	DATE		NOT NULL," + "\n" + 
+			"	PublicationID 	INT 			NOT NULL," + "\n" +
+			"	Type 			VARCHAR(16) 	NOT NULL," + "\n" + 
+			"	Title 			VARCHAR(128) 	NOT NULL," + "\n" + 
+			"	Issue 			INT 			NOT NULL," + "\n" + 
+			"	IssueDate 		DATE			NOT NULL," + "\n" + 
 			"	PRIMARY KEY (PublicationID)," + "\n" + 
 			"	FOREIGN KEY (PublicationID)" + "\n" + 
 			"		REFERENCES Publication (PublicationID)" + "\n" + 
@@ -150,10 +150,10 @@ public class InitDB {
 		/* ------------------------------------------------------------------ */
 		SQL = 
 			"CREATE TABLE Chapter (" + "\n" + 
-			"	PublicationID	INT 		NOT NULL," + "\n" + 
-			"	ChapterNumber 	INT 		NOT NULL," + "\n" + 
-			"	Title 		VARCHAR(128) 	NOT NULL," + "\n" + 
-			"	Text 		LONGTEXT 	NOT NULL," + "\n" + 
+			"	PublicationID	INT 			NOT NULL," + "\n" + 
+			"	ChapterNumber 	INT 			NOT NULL," + "\n" + 
+			"	Title 			VARCHAR(128) 	NOT NULL," + "\n" + 
+			"	Text 			LONGTEXT 		NOT NULL," + "\n" + 
 			"	PRIMARY KEY (PublicationID, ChapterNumber)," + "\n" +
 			"	FOREIGN KEY (PublicationID)" + "\n" +
 			"		REFERENCES Publication (PublicationID)" + "\n" + 
@@ -170,11 +170,11 @@ public class InitDB {
 		/* ------------------------------------------------------------------ */
 		SQL = 
 			"CREATE TABLE Article (" + "\n" +
-			"	PublicationID 	INT 		NOT NULL," + "\n" + 
-			"	SequenceNumber INT 		NOT NULL," + "\n" + 
-			"	Title 		VARCHAR(128) 	NOT NULL," + "\n" +
-			"	CreationDate 	DATE 		NOT NULL," + "\n" + 
-			"	Text 		MEDIUMTEXT 	NOT NULL," + "\n" +
+			"	PublicationID 		INT 			NOT NULL," + "\n" + 
+			"	SequenceNumber 		INT 			NOT NULL," + "\n" + 
+			"	Title 				VARCHAR(128) 	NOT NULL," + "\n" +
+			"	CreationDate 		DATE 			NOT NULL," + "\n" + 
+			"	Text 				MEDIUMTEXT 		NOT NULL," + "\n" +
 			"	PRIMARY KEY (PublicationID, SequenceNumber)," + "\n" + 
 			"	FOREIGN KEY (PublicationID)" + "\n" + 
 			"		REFERENCES Publication (PublicationID)" + "\n" + 
@@ -191,10 +191,15 @@ public class InitDB {
 		/* ------------------------------------------------------------------ */
 		SQL = 
 			"CREATE TABLE Contributor (" + "\n" + 
-			"	ContributorID	INT 		NOT NULL," + "\n" + 
-			"	Name 		VARCHAR(128) 	NOT NULL," + "\n" +
-			"	Position 	VARCHAR(16) 	NOT NULL," + "\n" + 
-			"	Type 		VARCHAR(16) 	NOT NULL," + "\n" + 
+			"	ContributorID	INT 			NOT NULL," + "\n" + 
+			"	Name 			VARCHAR(128) 	NOT NULL," + "\n" +
+			"	Position 		VARCHAR(16) 	NOT NULL," + "\n" + 
+			"	Type 			VARCHAR(16) 	NOT NULL," + "\n" + 
+			"	Age 			INT 			NOT NULL," + "\n" + 
+			"	Gender 			CHAR(1) 		NOT NULL," + "\n" + 
+			"	Phone 			CHAR(10) 		NOT NULL," + "\n" + 
+			"	Email 			VARCHAR(128) 	NOT NULL," + "\n" + 
+			"	Address			VARCHAR(128) 	NOT NULL," + "\n" + 
 			"	PRIMARY KEY (ContributorID)" + "\n" +
 			");" + "\n" + "\n" 
 		;
@@ -207,7 +212,7 @@ public class InitDB {
 		/* ------------------------------------------------------------------ */
 		SQL = 
 			"CREATE TABLE Edits (" + "\n" + 
-			"	PublicationID	INT	NOT NULL," + "\n" +
+			"	PublicationID	INT		NOT NULL," + "\n" +
 			"	ContributorID 	INT 	NOT NULL," + "\n" + 
 			"	PRIMARY KEY (PublicationID, ContributorID)," + "\n" +
 			"	FOREIGN KEY (PublicationID)" + "\n" + 
@@ -229,7 +234,7 @@ public class InitDB {
 		SQL = 
 			"CREATE TABLE AuthorsArticle (" + "\n" + 
 			"	PublicationID		INT 	NOT NULL," + "\n" + 
-			"	SequenceNumber		INT	NOT NULL," + "\n" + 
+			"	SequenceNumber		INT		NOT NULL," + "\n" + 
 			"	ContributorID 		INT 	NOT NULL," + "\n" + 
 			"	PRIMARY KEY (PublicationID, SequenceNumber, ContributorID)," + "\n" +
 			"	FOREIGN KEY (PublicationID, SequenceNumber)"+ "\n" +
@@ -272,7 +277,7 @@ public class InitDB {
 		SQL = 
 			"CREATE TABLE Address (" + "\n" + 
 			"	StreetAddress	VARCHAR(128) NOT NULL," + "\n" +
-			"	City 		VARCHAR(128) NOT NULL," + "\n" + 
+			"	City 			VARCHAR(128) NOT NULL," + "\n" + 
 			"	PRIMARY KEY (StreetAddress)" + "\n" +
 			");" + "\n"  + "\n"
 		;
@@ -284,13 +289,13 @@ public class InitDB {
 		/* ------------------------------------------------------------------ */
 		SQL = 
 			"CREATE TABLE Distributor (" + "\n" + 
-			"	DistributorID	INT 		NOT NULL," + "\n" + 
-			"	Name 		VARCHAR(128) 	NOT NULL," + "\n" +
-			"	Balance 	DECIMAL(14,2)	NOT NULL," + "\n" +
-			"	PhoneNumber 	CHAR(10) 	NOT NULL," + "\n" + 
+			"	DistributorID	INT 			NOT NULL," + "\n" + 
+			"	Name 			VARCHAR(128) 	NOT NULL," + "\n" +
+			"	Balance 		DECIMAL(14,2)	NOT NULL," + "\n" +
+			"	PhoneNumber 	CHAR(10) 		NOT NULL," + "\n" + 
 			"	StreetAddress 	VARCHAR(128) 	NOT NULL," + "\n" + 
 			"	ContactPerson 	VARCHAR(128) 	NOT NULL," + "\n" + 
-			"	Type 		VARCHAR(32) 	NOT NULL," + "\n" + 
+			"	Type 			VARCHAR(32) 	NOT NULL," + "\n" + 
 			"	PRIMARY KEY (DistributorID)," + "\n" + 
 			"	FOREIGN KEY (StreetAddress)" + "\n" + 
 			"		REFERENCES Address (StreetAddress)" + "\n" + 
@@ -305,14 +310,14 @@ public class InitDB {
 		/* ------------------------------------------------------------------ */
 		SQL = 
 			"CREATE TABLE Orders (" + "\n" + 
-			"	OrderID 	INT 		NOT NULL," + "\n" + 
-			"	DistributorID 	INT 		NOT NULL," + "\n" + 
-			"	PublicationID 	INT 		NOT NULL," + "\n" +
-			"	Units 		INT 		NOT NULL," + "\n" +
+			"	OrderID 		INT 			NOT NULL," + "\n" + 
+			"	DistributorID 	INT 			NOT NULL," + "\n" + 
+			"	PublicationID 	INT 			NOT NULL," + "\n" +
+			"	Units 			INT 			NOT NULL," + "\n" +
 			"	PricePerUnit 	DECIMAL(7,2)	NOT NULL," + "\n" + 
-			"	OrderDate 	DATE 		NOT NULL," + "\n" +
+			"	OrderDate 		DATE 			NOT NULL," + "\n" +
 			"	ShippingCost 	DECIMAL(9,2)	NOT NULL," + "\n" + 
-			"	Status 		VARCHAR(16) 	NOT NULL," + "\n" + 
+			"	Status 			VARCHAR(16) 	NOT NULL," + "\n" + 
 			"	PRIMARY KEY (OrderID)," + "\n" +
 			"	FOREIGN KEY (DistributorID)"+ "\n" +
 			"		REFERENCES Distributor (DistributorID)"+ "\n" +
@@ -331,9 +336,9 @@ public class InitDB {
 		/* ------------------------------------------------------------------ */
 		SQL = 
 			"CREATE TABLE Transaction (" + "\n" +  
-			"	TransactionID	INT 		NOT NULL," + "\n" + 
-			"	Amount 		DECIMAL(14,2) 	NOT NULL," + "\n" +
-			"	IssueDate 	DATE 		NOT NULL," + "\n" + 
+			"	TransactionID	INT 			NOT NULL," + "\n" + 
+			"	Amount 			DECIMAL(14,2) 	NOT NULL," + "\n" +
+			"	IssueDate 		DATE 			NOT NULL," + "\n" + 
 			"	PRIMARY KEY (TransactionID)" + "\n" +
 			");" + "\n" 
 		;
@@ -345,9 +350,9 @@ public class InitDB {
 		/* ------------------------------------------------------------------ */
 		SQL = 
 			"CREATE TABLE Bills (" + "\n" + "\t" +
-			"	TransactionID	INT		NOT NULL," + "\n" + 
+			"	TransactionID	INT			NOT NULL," + "\n" + 
 			"	DistributorID 	INT 		NOT NULL," + "\n" + 
-			"	Paid 		BOOL 		NOT NULL," + "\n" +
+			"	Paid 			BOOL 		NOT NULL," + "\n" +
 			"	PaymentDate 	DATE," + "\n" +
 			"	PRIMARY KEY (TransactionID)," + "\n" +
 			"	FOREIGN KEY (TransactionID)" + "\n" +
@@ -367,10 +372,10 @@ public class InitDB {
 		/* ------------------------------------------------------------------ */
 		SQL = 
 			"CREATE TABLE Wages (" + "\n" +
-			"	TransactionID	INT 		NOT NULL," + "\n" +
-			"	ContributorID 	INT 		NOT NULL," + "\n" + 
-			"	Type 		VARCHAR(32) 	NOT NULL," + "\n" + 
-			"	ClaimDate 	DATE," + "\n" + 
+			"	TransactionID	INT 			NOT NULL," + "\n" +
+			"	ContributorID 	INT 			NOT NULL," + "\n" + 
+			"	Type 			VARCHAR(32) 	NOT NULL," + "\n" + 
+			"	ClaimDate 		DATE," + "\n" + 
 			"	PRIMARY KEY (TransactionID)," + "\n" + 
 			"	FOREIGN KEY (TransactionID)" + "\n" +
 			"		REFERENCES Transaction (TransactionID)" + "\n" +
@@ -517,19 +522,19 @@ public class InitDB {
 		/* ------------------------------------------------------------------ */
 		SQL = 
 			"INSERT INTO Contributor VALUES "  + "\n" + "\t" +
-				"(1,  'J.K. Rowling', 'Author', 'Staff')," + "\n" + "\t" +
-				"(2,  'Kate Fagan', 'Author', 'Staff')," + "\n" + "\t" +
-				"(3,  'Martin Gilbert', 'Author', 'Invited')," + "\n" + "\t" +
-				"(4,  'James Clear', 'Author', 'Invited')," + "\n" + "\t" +
-				"(5,  'Bob Woodward', 'Author', 'Staff')," + "\n" + "\t" +
-				"(6,  'Barbara Walter', 'Author', 'Staff')," + "\n" + "\t" +
-				"(7,  'Anna Wintour', 'Editor', 'Staff')," + "\n" + "\t" +
-				"(8,  'Dasha Gold', 'Editor', 'Invited')," + "\n" + "\t" +
-				"(9,  'Emmanuelle', 'Editor', 'Staff')," + "\n" + "\t" +
-				"(10,  'Carine Roitfeld', 'Editor', 'Invited'),"  + "\n" + "\t" +
-				"(3001,  'John', 'Editor', 'Staff'),"  + "\n" + "\t" +
-				"(3002,  'Ethen', 'Editor', 'Staff'),"  + "\n" + "\t" +
-				"(3003,  'Cathy', 'Author', 'Invited')"  + "\n" +
+				"(1,  'J.K. Rowling', 'Author', 'Staff', 56, 'F', '9477488475', 'imawizard@potter.com', '1200 Hogwarts Street, ENG')," + "\n" + "\t" +
+				"(2,  'Kate Fagan', 'Author', 'Staff', 32, 'F', '8563156942', 'katefagan@gmail.com', '101 Main Street, NC 27513')," + "\n" + "\t" +
+				"(3,  'Martin Gilbert', 'Author', 'Invited', 45, 'M', '3685149657', 'martingilbert@gmail.com', '345 Silver Church Road, NC 27845')," + "\n" + "\t" +
+				"(4,  'James Clear', 'Author', 'Invited', 74, 'M', '4865123975', 'jameclear@gmail.com', '2306 Raleigh Road, NC 27530')," + "\n" + "\t" +
+				"(5,  'Bob Woodward', 'Author', 'Staff', 21, 'M', '9863254178', 'bossboby@gmail.com', '310 Carytown Road, NC 27513')," + "\n" + "\t" +
+				"(6,  'Barbara Walter', 'Author', 'Staff', 26, 'F', '6668453248', 'barbarawalter@yahoo.com', '598 Old Apex Road, NC 27516')," + "\n" + "\t" +
+				"(7,  'Anna Wintour', 'Editor', 'Staff', 28, 'F', '3877749865', 'annawintour@yahoo.com', '234 Wake Avenue, NC 28645')," + "\n" + "\t" +
+				"(8,  'Dasha Gold', 'Editor', 'Invited', 30, 'F', '5488675309', '24kgold@hotmail.com', '1602 Broadway, NY 15036')," + "\n" + "\t" +
+				"(9,  'Emmanuelle', 'Editor', 'Staff', 54, 'M', '55555555555', 'emmanuelle@hotmail.com', '9849 Higher Living Circle, Apt. 235, NC 27348')," + "\n" + "\t" +
+				"(10,  'Carine Roitfeld', 'Editor', 'Invited', 38, 'F', '9865321148', 'carineroitfeld@ncsu.edu', '666 Big Cow Road, TX 69420'),"  + "\n" + "\t" +
+				"(3001,  'John', 'Editor', 'Staff', 36, 'M', '9391234567', '3001@gmail.com', '21 ABC St, NC 27'),"  + "\n" + "\t" +
+				"(3002,  'Ethen', 'Editor', 'Staff', 30, 'M', '9491234567', '3002@gmail.com', '21 ABC St, NC 27606'),"  + "\n" + "\t" +
+				"(3003,  'Cathy', 'Author', 'Invited', 28, 'F', '9591234567', '3003@gmail.com', '3300 AAA St, NC 27606')"  + "\n" +
 			";" + "\n" + "\n"
 		;
 		System.out.println(SQL);
@@ -660,7 +665,10 @@ public class InitDB {
 				"(9, 300.00, '2022-03-10'),"  + "\n" + "\t" +
 				"(10, 630.00, '2020-01-02'),"  + "\n" + "\t" +
 				"(11, 215.00, '2020-02-05'),"  + "\n" + "\t" +
-				"(12, 115.00, '2020-02-10')"  + "\n" +
+				"(12, 115.00, '2020-02-10'),"  + "\n" + "\t" +
+				"(13, 1000.00, '2020-04-01'),"  + "\n" + "\t" +
+				"(14, 1000.00, '2020-04-01'),"  + "\n" + "\t" +
+				"(15, 1200.00, '2020-04-01')"  + "\n" + 
 			";" + "\n" + "\n"
 		;
 		System.out.println(SQL);
@@ -693,7 +701,10 @@ public class InitDB {
 				"(3, 1,  'Book Authorship', '2022-01-03')," + "\n" + "\t" +
 				"(4, 7, 'Editorial Work', '2022-01-05'),"  + "\n" + "\t" +
 				"(5, 1, 'Article Authorship', '2022-01-05'),"  + "\n" + "\t" +
-				"(9, 1, 'Article Authorship', NULL)"  + "\n" +
+				"(9, 1, 'Article Authorship', NULL),"  + "\n" + "\t" +
+				"(13, 3001, 'Editorial Work', '2020-04-02'),"  + "\n" + "\t" +
+				"(14, 3002, 'Editorial Work', '2020-04-02'),"  + "\n" + "\t" +
+				"(15, 3003, 'Editorial Work', '2020-04-02')"  + "\n" +
 			";" + "\n" + "\n"
 		;
 		System.out.println(SQL);
